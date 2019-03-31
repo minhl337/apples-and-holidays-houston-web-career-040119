5237aa87e85bbf4b6352e50709c229e9156c6db2
minh nguyen <imnotminh@gmail.com>
2019-03-31 16:42:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -118,7 +118,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     
     list_of_holidays.each do |holiday, supplies|
-      puts "#{holiday.to_s.split("_").map(&:capitalize).join(' ')}: #{supplies.join(', ')}  "
+      puts "#{holiday.to_s.split("_").map(&:capitalize).join(' ')}: #{supplies.join(', ')}"
 
     end
   end
