2164723dc2c98ede18abd45750bba870803d9331
minh nguyen <imnotminh@gmail.com>
2019-03-31 16:27:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -114,7 +114,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.each do |
+  holiday_hash.each do |season, list_of_holidays|
+    puts season.to_s
+    
+  end
 
   
 
